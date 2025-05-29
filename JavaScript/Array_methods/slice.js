91056916af9870d6122cf0d24516f4f39ff501e2
Disha Vadoliya <vdsh321@gmail.com>
2025-05-29 20:38:18 +0530

    Create slice.js

---
+++ JavaScript/Array_methods/slice.js
@@ -0,0 +1,10 @@
+const fruits =["apple" , "banana" ,"orange"];
+const sliced = fruits.slice(0,1);
+console.log(sliced);
+
+const lastTwo = fruits.slice(-2);
+console.log(lastTwo); 
+
+const message = "JavaScript";
+const sliced1 = message.slice(0, 3);
+console.log(sliced1); // "JavaScript"
