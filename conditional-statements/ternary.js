e980d87bf50cf016df81003e1074df7aa1553fa6
Disha Vadoliya <vdsh321@gmail.com>
2025-05-29 13:44:53 +0530

    Create ternary.js

---
+++ conditional-statements/ternary.js
@@ -0,0 +1,8 @@
+/*Check Voting Eligibility*/
+let age = parseInt(prompt("enter your age"));
+(age>=18)? console.log("eligible"):console.log("not eligible");
+
+//nested ternary
+/* Check if a Number is Positive, Negative, or Zero*/
+let num = parseInt(prompt("enter a number"));
+(num>0)? console.log("positive"):(num<0)? console.log("negative"):console.log("zero");
