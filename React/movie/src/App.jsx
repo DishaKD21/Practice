29057842fbd4a40000ae7df4ecce9d1dacae50a1
DishaKD21 <vdsh321@gmail.com>
2025-05-31 18:08:28 +0530

    added pages for favourites and joint it using routes

--- React/movie/src/App.jsx
+++ React/movie/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Home from "./pages/Home"
+import Favourites from "./pages/Favourites"
+import { Routes , Route} from "react-router-dom"
 function App() {
-
   return (
-    <>
-    <Home/>
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path='/' element= {<Home />} />
+        <Route path='/favourites' element={<Favourites />} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ React/movie/src/pages/Favourites.jsx
@@ -0,0 +1,8 @@
+function Favourites(){
+    return <div className="favouriters-empty">
+        <h2>No favourite movies yet</h2>
+        <p>start adding movies to your favourites and they will appear here</p>
+    </div>
+}
+
+export default Favourites ;
