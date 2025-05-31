4801f3af6ba9b88befb4617e3fbbbc09e9e6fea3
DishaKD21 <vdsh321@gmail.com>
2025-05-31 17:25:23 +0530

    Shows a search input box. Filters and displays movie cards based on user input (searchTerm). Uses a React useState hook to manage the value typed in the input.

--- React/movie/src/pages/Home.jsx
+++ React/movie/src/pages/Home.jsx
@@ -1,28 +1,40 @@
 import MovieCard from "../components/movieCard";
+import { useState } from "react"
+function Home() {
 
-function Home(){
    const movies = [
-  { id: 1, title: "John Wick", release_date: "2020", url: "https://via.placeholder.com/150" },
-  { id: 2, title: "Jhn Wick", release_date: "2020", url: "https://via.placeholder.com/150" },
-  { id: 3, title: "Joh Wick", release_date: "2020", url: "https://via.placeholder.com/150" },
-  { id: 4, title: "Jon Wick", release_date: "2020", url: "https://via.placeholder.com/150" },
-];
+      { id: 1, title: "John Wick", release_date: "2020", url: "https://via.placeholder.com/150" },
+      { id: 2, title: "Jhn Wick", release_date: "2020", url: "https://via.placeholder.com/150" },
+      { id: 3, title: "Joh Wick", release_date: "2020", url: "https://via.placeholder.com/150" },
+      { id: 4, title: "Jon Wick", release_date: "2020", url: "https://via.placeholder.com/150" },
+   ];
+  const [searchTerm, setSearchTerm] = useState("");
 
- const handleSearch = () => {};
+   const handleSearch = (e) => {
+      e.preventDefault(); // prevent page refresh
+      setSearchTerm("");
+   };
+   return (
+      <div className="home">
+         <form onSubmit={handleSearch} className="search-form">
+            <input type="text" 
+            placeholder="Search for movies..." 
+            className="search-input" 
+             value={searchTerm}
+            onChange = {(e) => setSearchTerm(e.target.value)}
+            />
+         
+            <button type="submit" className="search-button">Search</button>
+         </form>
 
-    return (
-    <div className="home">
-<form onSubmit={handleSearch} className="search-form">
-<input type="text" placeholder="Search for movies..." className="search-input"/>
-<button type="submit" className="search-button">Search</button>
-</form>
-
-   <div className="movie-grid">
-        {movies.map((movie)=>(
-           <MovieCard movie={movie} key={movie.id}/>
-        ))}
-    </div>
-    </div>
- );
+           <div className="movie-grid">
+        {movies.map((movie) => (
+          movie.title.toLowerCase().startsWith(searchTerm) && 
+          (<MovieCard movie={movie} key={movie.id} />)
+        )
+        )}
+      </div>
+      </div>
+   );
 }
 export default Home;
