0556b9c7d280fbeb4e54b5c0e2ef20ddc1d41842
DishaKD21 <vdsh321@gmail.com>
2025-06-02 11:07:51 +0530

    created task manager app

---
+++ React/Task_Manager/Task_manager/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+   <>
+   <h1>Simple Task Manager</h1>
+    
+   </>
+  )
+}
+
+export default App;
---
+++ React/Task_Manager/Task_manager/src/components/add_task.jsx
@@ -0,0 +1,8 @@
+function Add_Task(){
+    return(
+        <>
+        
+        </>
+    );
+}
+export default Add_Task;
