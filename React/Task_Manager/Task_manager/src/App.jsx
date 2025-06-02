ca9e679c61a37c2b8299873d3ade63c960ccb6a5
DishaKD21 <vdsh321@gmail.com>
2025-06-02 11:16:18 +0530

    created add_task component and added text_field button ,select input fields

--- React/Task_Manager/Task_manager/src/App.jsx
+++ React/Task_Manager/Task_manager/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import Add_Task from './components/add_task';
 function App() {
  
   return (
    <>
    <h1>Simple Task Manager</h1>
-    
+    <Add_Task></Add_Task>
    </>
   )
 }
--- React/Task_Manager/Task_manager/src/components/add_task.jsx
+++ React/Task_Manager/Task_manager/src/components/add_task.jsx
@@ -1,8 +1,16 @@
 function Add_Task(){
     return(
-        <>
-        
-        </>
+        <div className="container">
+        <h2>Add New Task</h2>
+        <input type="text" />
+        <br />
+        <select name="Select" id="">
+         <option value="Personal">Personal</option>
+         <option value="Work">Work</option>
+        </select>
+        <br/>
+        <button> + Add Task</button>
+        </div>  
     );
 }
 export default Add_Task;
