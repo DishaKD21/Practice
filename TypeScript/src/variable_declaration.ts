e953c3e3497f8b92cd59173c2b06376f6eefaa57
DishaKD21 <vdsh321@gmail.com>
2025-06-09 15:11:13 +0530

    variable declaration

---
+++ TypeScript/src/variable_declaration.ts
@@ -0,0 +1,9 @@
+console.log("hello world");
+// JavaScript
+// let message = "hello";
+// message = 123;  // Allowed, no error
+
+// TypeScript
+let message: string = "hello";
+// message = 123;  // Error: Type 'number' is not assignable to type 'string'
+message = "heyyyaa"; //this is correct 
