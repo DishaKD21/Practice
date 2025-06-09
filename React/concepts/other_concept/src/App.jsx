4689fa1395d0f7ffc41a334a8801dd00719dc6eb
DishaKD21 <vdsh321@gmail.com>
2025-06-09 17:42:55 +0530

    created new project

---
+++ React/concepts/other_concept/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-amber-300">
+      <p className="text-7xl text-center">
+        hii
+      </p>
+    </div>
+  )
+}
+
+export default App
