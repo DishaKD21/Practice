6575f2445fffab821eb934b30b6b858abafce5f0
DishaKD21 <vdsh321@gmail.com>
2025-06-11 15:05:55 +0530

    practice of useEffect

--- React/concepts/TypesOfHooks/src/App.jsx
+++ React/concepts/TypesOfHooks/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import UseEffect from './components/Useeffect';
+import LoggerComponent from './components/LoggerComponent'
 function App() {
   return (
     <>
     <UseEffect></UseEffect>
+    <LoggerComponent></LoggerComponent>
     </>
   )
 }
