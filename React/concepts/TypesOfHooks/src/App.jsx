af0a40c043ae61e152924cf668d5125b570c4618
DishaKD21 <vdsh321@gmail.com>
2025-06-12 13:23:37 +0530

    added routing

--- React/concepts/TypesOfHooks/src/App.jsx
+++ React/concepts/TypesOfHooks/src/App.jsx
@@ -2,12 +2,37 @@ import './App.css';
 import UseEffect from './components/Useeffect';
 import LoggerComponent from './components/LoggerComponent'
 import UseContext from './components/UseContext'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import Home from './components/Home';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+const router = createBrowserRouter(
+  [
+    {
+     path: "/",
+     element : <div><Navbar/><Home/></div>
+    },
+    
+    {
+     path: "/about",
+     element :  <div><Navbar/><About/></div>
+    },
+
+    {
+     path: "/dashboard",
+     element : <div><Navbar/><Dashboard/></div>
+    }
+  ]
+);
 function App() {
   return (
     <>
     <UseEffect></UseEffect>
     <LoggerComponent></LoggerComponent>
     <UseContext></UseContext>
+    < RouterProvider router={router} />
+ 
     </>
   )
 }
